754bdc95e5938ec60d4d5471028a841234f7bb5e
inskandar678 <89117432+inskandar678@users.noreply.github.com>
2021-09-10 18:46:44 +0800

    index edit

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const qrcode = require('qrcode');
 const http = require('http');
 const fs = require('fs');
 const { phoneNumberFormatter } = require('./helpers/formatter');
-const fileUpload = require('express-fileupload');
+
 const axios = require('axios');
+const port = process.env.PORT || 8000;
 const mime = require('mime-types');
 
-const port = process.env.PORT || 8000;
+
 
 const app = express();
 const server = http.createServer(app);
@@ -20,9 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(fileUpload({
-  debug: true
-}));
+
 
 const SESSION_FILE_PATH = './whatsapp-session.json';
 let sessionCfg;
@@ -52,6 +51,53 @@ const client = new Client({
     ],
   },
   session: sessionCfg
+});
+
+
+client.initialize();
+
+// Socket IO
+io.on('connection', function(socket) {
+  socket.emit('message', 'Menghubungkan....');
+
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code Diterima,silahkan di scan !');
+    });
+  });
+
+  client.on('ready', () => {
+    socket.emit('ready', 'Whatsapp Bot terhubung!');
+    socket.emit('message', 'Whatsapp Bot terhubung!');
+  });
+
+  client.on('authenticated', (session) => {
+    socket.emit('authenticated', 'Whatsapp di auntentikasi!');
+    socket.emit('message', 'Whatsapp di auntentikasi!');
+    console.log('AUTHENTICATED', session);
+    sessionCfg = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+
+  client.on('auth_failure', function(session) {
+    socket.emit('message', 'autentikasi gagal,memulai ulang');
+  });
+
+  client.on('disconnected', (reason) => {
+    socket.emit('message', 'Whatsapp Bot terputus!');
+    fs.unlinkSync(SESSION_FILE_PATH, function(err) {
+        if(err) return console.log(err);
+        console.log('Session file deleted!');
+    });
+    client.destroy();
+    client.initialize();
+  });
 });
 
 //pesan//
@@ -205,57 +251,9 @@ client.on('message', message => { //HALO//
 });
 
 //end of pesan//
-client.initialize();
-
-// Socket IO
-io.on('connection', function(socket) {
-  socket.emit('message', 'Connecting...');
-
-  client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
-    qrcode.toDataURL(qr, (err, url) => {
-      socket.emit('qr', url);
-      socket.emit('message', 'QR Code received, scan please!');
-    });
-  });
-
-  client.on('ready', () => {
-    socket.emit('ready', 'Whatsapp is ready!');
-    socket.emit('message', 'Whatsapp is ready!');
-  });
-
-  client.on('authenticated', (session) => {
-    socket.emit('authenticated', 'Whatsapp is authenticated!');
-    socket.emit('message', 'Whatsapp is authenticated!');
-    console.log('AUTHENTICATED', session);
-    sessionCfg = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
-      if (err) {
-        console.error(err);
-      }
-    });
-  });
-
-  client.on('auth_failure', function(session) {
-    socket.emit('message', 'Auth failure, restarting...');
-  });
-
-  client.on('disconnected', (reason) => {
-    socket.emit('message', 'Whatsapp is disconnected!');
-    fs.unlinkSync(SESSION_FILE_PATH, function(err) {
-        if(err) return console.log(err);
-        console.log('Session file deleted!');
-    });
-    client.destroy();
-    client.initialize();
-  });
-});
 
 
-const checkRegisteredNumber = async function(number) {
-  const isRegistered = await client.isRegisteredUser(number);
-  return isRegistered;
-}
+
 
 server.listen(port, function() {
 	console.log('App running on *:' + port);
